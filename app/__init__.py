a26631315724f7c2006cf89b2428517c9cc50fa0
pooja <pooja.sunagar@esthenos.com>
2021-12-16 20:51:37 +0530

    flask commit-2

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'you-will-never-guess'
-# ... add more variables here as needed
+app.config.from_object(Config)
 
 from app import routes
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
