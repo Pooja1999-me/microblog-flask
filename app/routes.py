59709294382fb7bebe4b4fd16f525eaf99421336
pooja <pooja.sunagar@esthenos.com>
2021-12-29 16:16:44 +0530

    User Profile Page

--- app/routes.py
+++ app/routes.py
@@ -28,13 +28,13 @@ def login():
         return redirect(next_page)
 
 
-@app.route('/')
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
 
-@app.route('/')
+
 @app.route('/index')
 @login_required
 def index():
@@ -42,7 +42,7 @@ def index():
     return render_template("index.html", title='Home Page', posts=posts)
 
 
-@app.route('/')
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
@@ -56,3 +56,13 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
